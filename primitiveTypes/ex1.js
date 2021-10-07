75ce03c2eace73e1eabfa7e6f0081a0182db91d8
Lucas Fernando <lucas98fernando@gmail.com>
2021-10-06 21:41:05 -0300

    Exemplo de Number

---
+++ primitiveTypes/ex1.js
@@ -0,0 +1,13 @@
+const numberValorMaximo = Number.MAX_VALUE
+const numberValorMinimo = Number.MIN_VALUE
+const maxSafeInteger = Number.MAX_SAFE_INTEGER
+const minSafeInteger = Number.MIN_SAFE_INTEGER
+const isSafeInteger = Number.isSafeInteger()
+
+console.log(numberValorMaximo)
+console.log(numberValorMinimo)
+console.log(maxSafeInteger)
+console.log(minSafeInteger)
+console.log(isSafeInteger)
+
+// BigInt se comporta de maneira semelhante ao Number em caso que é convertido para Boolean, como por exemplo: (if, ||, &&, Boolean, !)
